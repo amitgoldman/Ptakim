43c53e7c3e11381fb498816d61a04dd80c7f3a03
amit goldman <amitgoldman11@gmail.com>
2021-04-12 10:45:13 +0300

    swagger added

--- Ptakim/settings.py
+++ Ptakim/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3th party apps
+    'drf_yasg',
     'rest_framework',
+
     # My apps
     'Tasks',
 ]
--- Tasks/views.py
+++ Tasks/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from Tasks.serializer import TaskSerializer
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from Tasks.models import Task
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @action(detail=True, methods=['patch'])
+    def change_user(self, request, pk=None):
+        pass
+
